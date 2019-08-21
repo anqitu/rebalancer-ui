05ffeb92fa62f2443b9042d48c981d7afdef960d
Anqi Tu <anqitu@outlook.com>
2019-08-21 14:48:51 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rbc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rebalancer-ui';
+}
